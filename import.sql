8d2f89c1525aba26ab1bc90a70f7f3423bb9e90c
M.ZAKY ARISANDHI <55038401+kikik27@users.noreply.github.com>
2022-03-11 20:47:24 +0700

    Add files via upload

---
+++ import.sql
@@ -0,0 +1,202 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 22, 2021 at 08:20 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `penjualan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`username`, `password`) VALUES
+('admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang_keluar`
+--
+
+CREATE TABLE `barang_keluar` (
+  `NoTransaksi` varchar(25) NOT NULL,
+  `nama_customer` varchar(25) NOT NULL,
+  `tanggal` varchar(30) NOT NULL,
+  `id_barang` varchar(20) NOT NULL,
+  `nama_barang` varchar(25) NOT NULL,
+  `harga_barang` int(25) NOT NULL,
+  `Qty` int(25) NOT NULL,
+  `total` int(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Triggers `barang_keluar`
+--
+DELIMITER $$
+CREATE TRIGGER `barang_keluar` AFTER INSERT ON `barang_keluar` FOR EACH ROW BEGIN
+	UPDATE data_barang SET stok_barang=stok_barang-NEW.Qty
+    WHERE id_barang = NEW.id_barang;
+    END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang_masuk`
+--
+
+CREATE TABLE `barang_masuk` (
+  `tanggal` varchar(25) NOT NULL,
+  `id_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(30) NOT NULL,
+  `stok_barang` int(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Triggers `barang_masuk`
+--
+DELIMITER $$
+CREATE TRIGGER `barang_masuk` AFTER INSERT ON `barang_masuk` FOR EACH ROW BEGIN
+	UPDATE data_barang SET stok_barang=stok_barang+NEW.stok_barang
+    WHERE id_barang = NEW.id_barang;
+    END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_barang`
+--
+
+CREATE TABLE `data_barang` (
+  `id_barang` varchar(25) NOT NULL,
+  `nama_barang` varchar(25) DEFAULT NULL,
+  `harga_barang` int(11) DEFAULT NULL,
+  `stok_barang` int(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_customer`
+--
+
+CREATE TABLE `data_customer` (
+  `nama_customer` varchar(20) NOT NULL,
+  `alamat` varchar(20) NOT NULL,
+  `nomor_hp` varchar(20) NOT NULL,
+  `bon` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`username`, `password`) VALUES
+('user', 'user'),
+('user123', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `print`
+--
+
+CREATE TABLE `print` (
+  `NoTransaksi` varchar(20) NOT NULL,
+  `GrandTotal` int(20) NOT NULL,
+  `Tunai` int(20) NOT NULL,
+  `Kembalian` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `print2`
+--
+
+CREATE TABLE `print2` (
+  `NoTransaksi` varchar(30) NOT NULL,
+  `nama_customer` varchar(30) NOT NULL,
+  `tanggal` varchar(30) NOT NULL,
+  `id_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(30) NOT NULL,
+  `harga_barang` int(30) NOT NULL,
+  `Qty` int(30) NOT NULL,
+  `total` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang_keluar`
+--
+ALTER TABLE `barang_keluar`
+  ADD KEY `NoTransaksi` (`NoTransaksi`);
+
+--
+-- Indexes for table `data_barang`
+--
+ALTER TABLE `data_barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `data_customer`
+--
+ALTER TABLE `data_customer`
+  ADD PRIMARY KEY (`nama_customer`);
+
+--
+-- Indexes for table `print`
+--
+ALTER TABLE `print`
+  ADD PRIMARY KEY (`NoTransaksi`),
+  ADD KEY `NoTransaksi` (`NoTransaksi`,`GrandTotal`,`Tunai`,`Kembalian`),
+  ADD KEY `NoTransaksi_2` (`NoTransaksi`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
